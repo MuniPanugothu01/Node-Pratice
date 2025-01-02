6d1a738b93ecb0c4ab06562c668a6803c933f2e5
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-02 17:02:23 +0530

    parse method

--- os.js
+++ os.js
@@ -14,6 +14,7 @@ const path = require("path")
 console.log(path.dirname(__filename),"directory name");
 console.log(path.basename(__filename),"it give only file name!");
 console.log(path.extname(__filename),`"it gives the file extension name like .html, .css"`);
+console.log(path.parse(__filename));
 
 
 
