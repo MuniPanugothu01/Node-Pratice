cf3437b1b53e2e5b7c48c5afe65f1d352e10578b
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-03 12:26:39 +0530

    fs module pratice completed

--- fs.js
+++ fs.js
@@ -1,4 +1,4 @@
-const fs = require("fs")
+const fs = require("fs").promises
 // 1.read file we can read the data only can't acces the data while using the readFile
 // fs.readFile("example.txt", "utf-8", (err, data) => {
 //     if (err) {
@@ -78,3 +78,93 @@ const fs = require("fs")
 
 // });
 
+
+
+// 6. checking if file exists
+// fs.access('new_file.txt', fs.constants.F_OK, (err) => {
+//     if (err) {
+//         console.error('File does not exist');
+//     } else {
+//         console.log('File exists');
+//     }
+// });
+
+
+// 7.creating a directory
+
+// fs.mkdir('Second', { recursive: true }, (err) => {
+//     if (err) {
+//         console.error('Error creating directory:', err);
+//     } else {
+//         console.log('Directory created successfully');
+//     }
+// });
+
+
+// unlink the above both folder
+// const folder = "newFolder"
+// fs.rm(folder, { recursive: true, force: true }, (err) => {
+//     if (err) {
+//         console.log("file doesn't delete!", err);
+//     }
+//     else {
+//         console.log("file deleted!");
+//     }
+// })
+
+
+// deleted the second folder through mkdir method
+// const second = 'Second'
+// fs.rm(second,{recursive:true,force:true},(err)=>{
+//     if(err){
+//         console.log(err,"folder doesn't deleted!");
+//     }
+//     else{
+//         console.log('folder deleted succesfully');
+//     }    
+// })
+
+
+
+// 8. Reading a directory
+
+// fs.readdir('./', (err, files) => {
+//     if (err) {
+//         console.error('Error reading directory:', err);
+//     } else {
+//         console.log('Files in directory:', files);
+//     }
+// });
+
+
+
+// 9.watching a file for changes
+// const file = 'second.txt'
+// fs.watch(file, (eventType, filename) => {
+//     if (filename) {
+//         console.log(`File ${filename} has been ${eventType}`);
+//     } else {
+//         console.log('Filename not provided');
+//     }
+// });
+
+
+
+// getting file stats
+// fs.stat('second.txt', (err, stats) => {
+//     if (err) {
+//         console.error('Error getting file stats:', err);
+//     } else {
+//         console.log('File stats:', stats);
+//     }
+// });
+
+
+
+// promises asynchronus using 
+fs.readFile("second.txt", 'utf-8').then((data) => {
+    console.log("data displayed!", data);
+}).catch((err) => {
+    console.log(err, "data doesn't dispalyed!");
+
+})
