7213166acde56bfb45f63c628322979f80e82902
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-03 16:35:19 +0530

    http modules

--- http.js
+++ http.js
@@ -1,11 +1,26 @@
-const http = require("http")
-const url = require("url")
+const http = require("http");
+const fs = require("fs");
 // create the surver
 const server = http.createServer((req, res) => {
-    res.write("server created!")
-    res.write(req.url)
-    console.log(req.url);
-    res.end()
+    const Data = `${Date.now()} data recervied\n`
+    fs.appendFile("today.txt", Data, (err, data) => {
+        // res.write("when ever hit the server the data will store in file");
+        if (req.url == "/") {
+            res.write("home section")
+        }
+        else if (req.url == "/about") {
+            res.write("About section!")
+        }
+        else if (req.url == "/contact-us") {
+            res.write("contact us section")
+        }
+        else {
+            res.write("data not found!")
+            res.end()
+        }
+        res.end()
+    })
+
 
 })
 server.listen(3001, () => {
