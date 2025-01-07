b2ad86d8d83efbc12d652826c322ca59b6d3d501
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-07 06:07:52 +0530

    http methods

--- http2.js
+++ http2.js
@@ -1,26 +1,33 @@
 const http = require("http");
 const fs = require("fs");
-const url = require("url")
+const url = require("url");
+const { parse } = require("path");
 
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    res.write("another sections")
-    res.end()
-    let Data = `${Date.now()} new data recervised\n `
-    fs.appendFile("http.txt", Data,(err, data) => {
-        if (err) {
-            // res.writeHead(500, { "Content-Type": "text/plain" });
-            res.write(`Error writing to file: ${err.message}`);
+    let Data = `${Date.now()} new data recervised\n `;
+    const parsed = url.parse(req.url, true);
+    fs.appendFile("http.txt", Data, (err, data) => {
+        if (parsed.pathname === "/") {
+            res.write(`Home section!`);
+        }
+        else if (parsed.pathname === "/about") {
+            const username = parsed.query.myname;
+            res.write(`Hai ${username}`);
+        }
+        else if (parsed.pathname === "/search") {
+            const search = parsed.query.userid
+            res.write(`user id is! ${search}`)
         }
         else {
-            // res.writeHead(200, { "Content-Type": "text/plain" });
-            res.write("Data written to file successfully!");
+            const username = parsed.query.myname
+            res.write(`invalid user creditional ${username}`)
         }
         res.end()
     })
 
-    
+
 
 
 
