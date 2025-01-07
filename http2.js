c2f3900b0cee8950402f1bdd6e401a69ced5674a
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-07 11:09:05 +0530

    changes

--- http2.js
+++ http2.js
@@ -3,37 +3,27 @@ const fs = require("fs");
 const url = require("url");
 const { parse } = require("path");
 
-
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    let Data = `${Date.now()} new data recervised\n `;
-    const parsed = url.parse(req.url, true);
-    fs.appendFile("http.txt", Data, (err, data) => {
-        if (parsed.pathname === "/") {
-            res.write(`Home section!`);
-        }
-        else if (parsed.pathname === "/about") {
-            const username = parsed.query.myname;            
-            res.write(`Hai ${username}`);
-        }
-        else if (parsed.pathname === "/search") {
-            const search = parsed.query.userid;
-            res.write(`user id is! ${search}`)
-        }
-        else {
-            const username = parsed.query.myname
-            res.write(`invalid user creditional ${username}`)
-        }
-        res.end()
-    })
-
-
-
-
-
-})
+  console.log(req.url);
+  let Data = `${Date.now()} new data recervised\n `;
+  const parsed = url.parse(req.url, true);
+  fs.appendFile("http.txt", Data, (err, data) => {
+    if (parsed.pathname === "/") {
+      res.write(`Home section!`);
+    } else if (parsed.pathname === "/about") {
+      const username = parsed.query.myname;
+      res.write(`Hai ${username}`);
+    } else if (parsed.pathname === "/search") {
+      const search = parsed.query.userid;
+      res.write(`user id is! ${search}`);
+    } else {
+      const username = parsed.query.myname;
+      res.write(`invalid user creditional ${username}`);
+    }
+    res.end();
+  });
+});
 
 server.listen(3002, () => {
-    console.log(`port is running is! ${3002}`);
-
-})
+  console.log(`port is running is! ${3002}`);
+});
