6d25d89fbb452a5c5aa1168b214fd6b55fc9ede3
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-10 21:39:01 +0530

    ss

--- import.js
+++ import.js
@@ -1,12 +1,29 @@
+// const http = require("http");
+// // import the files
+// const { Add as ADD, Sub, Mul, Div } = require("./export");
+// console.log(ADD(10, 20)); // 30
+// console.log(Sub(20, 10)); // 10
+// console.log(Mul(2, 5)); // 10
+// console.log(Div(20, 5)); // 4
+// const server = http.createServer((req, res) => {
+//   res.write("here sendiing the data to server to display in broweser!");
+// });
+
+// server.listen(3001, () => {
+//   console.log(`Here port is! ${3001} Running`);
+// });
 const http = require("http");
 // import the files
 const { Add as ADD, Sub, Mul, Div } = require("./export");
-console.log(Add(10, 20)); // 30
+
+console.log(ADD(10, 20)); // 30
 console.log(Sub(20, 10)); // 10
 console.log(Mul(2, 5)); // 10
 console.log(Div(20, 5)); // 4
+
 const server = http.createServer((req, res) => {
-  res.write("here sendiing the data to server to display in broweser!");
+  res.write("Here sending the data to server to display in browser!");
+  res.end(); // End the response
 });
 
 server.listen(3001, () => {
