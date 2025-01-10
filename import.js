232187f4031c64c2d869e8e3101ab74616c25ff0
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-10 21:41:00 +0530

    add

--- import.js
+++ import.js
@@ -1,7 +1,7 @@
 const http = require("http");
 // import the files
 const { Add, Sub, Mul, Div } = require("./export");
-console.log(ADD(10, 20)); // 30
+console.log(Add(10, 20)); // 30
 console.log(Sub(20, 10)); // 10
 console.log(Mul(2, 5)); // 10
 console.log(Div(20, 5)); // 4
