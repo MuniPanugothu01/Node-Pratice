d6393fb255fe326db49aa5eb11991b2a21ad2059
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-01-17 22:38:02 +0530

    sending request to server!

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("this is the express!");
 });
+app.get('/about',(req,res)=>{
+    res.send("this is the about-us section!")
+});
+app.post('/contact',(req,res)=>{
+    res.send("this is the contact section!")
+})
 
 let port = 3001;
 app.listen(port, () => {
