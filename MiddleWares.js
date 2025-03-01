9096baba05bb1b8a734e21ce4f0ad59ea30acca2
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-01 07:05:10 +0530

    second app2 is created and also code running properly with middlewares

--- MiddleWares.js
+++ MiddleWares.js
@@ -5,7 +5,7 @@ const app2 = express();
 
 // middle wares, if the conditon will statify then only send the requst to server other wise conditin will false then it won't sent to server.
 app.use((req, res, next) => {
-  if (10 < 20) {
+  if (10 > 20) {
     next();
   } else {
     console.log("cont sent the request to server beacuse condition is false");
@@ -37,24 +37,25 @@ let first = (req, res, next) => {
     next();
   } else {
     console.log("request not send to server bez condition is false!");
+    res.status(403).send("Forbidden: Condition not met");
   }
 };
 
 // second API server router, is (app2)
 
-app.post("/home", first, (req, res) => {
+app2.post("/home", first, (req, res) => {
   res.send("data updated to server");
 });
 
-app.get("/second", (req, res) => {
+app2.get("/get", (req, res) => {
   res.send("server data get!");
 });
 
-app.put("/put", (req, res) => {
+app2.put("/put", (req, res) => {
   res.send("data updated!");
 });
 
-app.patch("/patch", (req, res) => {
+app2.patch("/patch", (req, res) => {
   res.send("data updated specific!");
 });
 
