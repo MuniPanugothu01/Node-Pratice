99e9c80f5e6466a81fa1bc94e0589c382779361a
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-04 14:36:52 +0530

    example completed on application level middle ware function

--- Application-level.js
+++ Application-level.js
@@ -1,3 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+// Application-level MiddlWare function
+app.use((req, res, next) => {
+  console.log(`request Method: ${req.method} and the urkl is ${req.url}`);
+  next(); // it pass the next function or router
+});
+
+app.get("/", (req, res) => {
+  res.send("this is the home section!");
+});
+
+app.post("/about", (req, res) => {
+  res.send("this is the post method!");
+});
+
+// assignin the port to the server or app
+let PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`port is the ${PORT} is connected!`);
+});
