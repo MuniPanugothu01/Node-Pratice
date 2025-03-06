fb064ab3ee168b0f45cc642937573eddf2b65048
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-06 11:37:10 +0530

    express installed

--- Application-level.js
+++ Application-level.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 // Application-level MiddlWare function
 app.use((req, res, next) => {
   console.log(`request Method: ${req.method} and the urkl is ${req.url}`);
@@ -22,33 +21,32 @@ app.listen(PORT, () => {
 });
 
 // second example on application level middleware.
+const app2 = express(); // Initialize express
 
-const app2 = require("express");
-
-// middleware
+// Middleware function
 let MiddleHandleFunction = (req, res, next) => {
-  console.log(`request method is ${req.method} and the url is ${req.url}`);
+  console.log(`Request method is ${req.method} and the URL is ${req.url}`);
   next();
 };
 
+// Use middleware
 app2.use(MiddleHandleFunction);
 
+// Define routes
 app2.get("/", (req, res) => {
-  res.send("this is home second app21");
+  res.send("This is home - second app21");
 });
 app2.post("/about", (req, res) => {
-  res.send("this is the about section for app22");
+  res.send("This is the about section for app22");
 });
-
 app2.put("/put", (req, res) => {
-  res.send("this is the put method!app22");
+  res.send("This is the PUT method! app22");
 });
 
+// Start the server
 const Port = 3002;
 app2.listen(Port, () => {
-  console.log(`port is connected ${Port}`);
+  console.log(`Server is running on port ${Port}`);
 });
-
-
 
 // Router level middle-Ware;
