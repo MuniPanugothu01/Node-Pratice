2d917c4048dce4ae7d4d82549e72fe05a9119f9e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-11 12:35:40 +0530

    app.get

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
+app.get("/home", (req, res) => {
+  res.send("this is home section");
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`port is connceted! ${PORT} runnig here`);
