87d3a9c6220c38c3aee4fffca403b6ca78ef80d8
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-11 12:34:05 +0530

    ff

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const dotEnv = require("dotenv");
-
+// const dotEnv = require("dotenv");
+require("dotenv").config();
 const app = express();
 
-app.listen(prompt, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log(`port is connceted! ${PORT} runnig here`);
 });
