f3a20358d9ee2312ed163baf1da1eeedfe910b7e
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-03-13 06:06:49 +0530

    my project to mongodb connected successfully

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
-// const dotEnv = require("dotenv");
-require("dotenv").config();
+const dotEnv = require("dotenv");
+// require("dotenv").config();
+dotEnv.config();
+
+// mongoose client require
+
+const { MongoClient } = require("mongodb");
+
 const app = express();
+
+MongoClient.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongodb connected successfully");
+  })
+  .catch(() => {
+    console.log("mongodb not connected!");
+  });
 
 app.get("/home", (req, res) => {
   res.send("this is home section");
 });
+
+console.log(process.env);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
